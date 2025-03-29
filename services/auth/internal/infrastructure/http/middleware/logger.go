0f4d2f4583ec31ef0947c499fbea7216a3f7fe0c
Klimenko George <george_klimenko6@mail.ru>
2025-03-29 15:29:29 +0300

    Инициализация структуры LoggerMiddleware

--- services/auth/internal/infrastructure/http/middleware/logger.go
+++ services/auth/internal/infrastructure/http/middleware/logger.go
@@ -2,6 +2,8 @@ package middleware
 
 import (
 	"net/http"
+
+	"github.com/klimenkokayot/avito-go/libs/logger"
 )
 
 type responseWriter struct {
@@ -12,4 +14,12 @@ type responseWriter struct {
 func (rw *responseWriter) WriteHeader(code int) {
 	rw.status = code
 	rw.WriteHeader(code)
+}
+
+func LoggerMiddleware(logger logger.Logger) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		logger.Debug("Request started")
+		logger.Debug("Request done")
+		return h
+	}
 }
