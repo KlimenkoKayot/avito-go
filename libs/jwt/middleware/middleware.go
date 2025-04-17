7f678904a5dd8fa01e827406f765c2a37172f547
Klimenko George <george_klimenko6@mail.ru>
2025-04-17 23:07:49 +0300

    Правки в AccessMiddleware

--- libs/jwt/middleware/middleware.go
+++ libs/jwt/middleware/middleware.go
@@ -16,39 +16,36 @@ func NewTokenMiddleware(tokenManager *jwt.TokenManager) (*TokenMiddleware, error
 	}, nil
 }
 
-func (a *TokenMiddleware) refreshTokenPair(r *http.Request) error {
+func (tm *TokenMiddleware) updateTokenPair(r *http.Request) error {
+	accessTokenCookie, err := r.Cookie("access_token")
+	if err != nil {
+		return ErrAccessTokenNotFound
+	}
+	accessToken := accessTokenCookie.Value
+
 	refreshTokenCookie, err := r.Cookie("refresh_token")
-	refreshTokenString := refreshTokenCookie.String()
-
-	claims, err := a.tokenManager.ParseWithClaims(refreshTokenString)
 	if err != nil {
-		return err
+		return ErrRefreshTokenNotFound
 	}
+	refreshToken := refreshTokenCookie.Value
 
-	login := (*claims)["lgn"].(string)
-	ip := r.RemoteAddr
-
-	newAccessTokenString, err := a.tokenManager.NewAccessToken(login, ip)
-	if err != nil {
-		return err
-	}
-	newRefreshTokenString, err := a.tokenManager.NewRefreshToken(login)
+	accessToken, refreshToken, err = tm.tokenManager.UpdateTokenPair(refreshToken, r.RemoteAddr)
 	if err != nil {
 		return err
 	}
 
 	r.AddCookie(&http.Cookie{
 		Name:  "access_token",
-		Value: newAccessTokenString,
+		Value: accessToken,
 	})
 	r.AddCookie(&http.Cookie{
 		Name:  "refresh_token",
-		Value: newRefreshTokenString,
+		Value: refreshToken,
 	})
 	return nil
 }
 
-func (a *TokenMiddleware) Middleware(next http.Handler) http.Handler {
+func (a *TokenMiddleware) AccessMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		accessTokenCookie, err := r.Cookie("access_token")
 		if err != nil {
@@ -64,41 +61,19 @@ func (a *TokenMiddleware) Middleware(next http.Handler) http.Handler {
 		}
 		refreshTokenString := refreshTokenCookie.String()
 
-		if accessTokenString == "" {
-			if refreshTokenString == "" {
-				http.Error(w, "refresh_token пустой", http.StatusUnauthorized)
-				return
-			} else if a.refreshTokenPair(r) != nil {
-				http.Error(w, "Не удалось обновить пару", http.StatusUnauthorized)
-				return
+		valid, err := a.tokenManager.ValidateTokenExpiration(accessTokenString)
+		if valid {
+			next.ServeHTTP(w, r)
+		}
+
+		valid, err = a.tokenManager.ValidateTokenExpiration(refreshTokenString)
+		if valid {
+			if err = a.updateTokenPair(r); err != nil {
+				next.ServeHTTP(w, r)
 			}
-			// Токены обновлены
 		}
 
-		valid, err := a.tokenManager.ValidateToken(accessTokenString)
-		if !valid {
-			http.Error(w, "Некорректный access_token", http.StatusUnauthorized)
-			return
-		}
-
-		claims, err := a.tokenManager.ParseWithClaims(accessTokenString)
-		if err != nil {
-			http.Error(w, "Ошибка во время парсинга данных access_token", http.StatusUnauthorized)
-			return
-		}
-
-		// login := (*claims)["lgn"].(string)
-		ip := (*claims)["uip"].(string)
-
-		if r.RemoteAddr != ip {
-			http.Error(w, "IP адреса не совпадают", http.StatusUnauthorized)
-			return
-		}
-
-		if err = a.refreshTokenPair(r); err != nil {
-			http.Error(w, "Не удалось обновить пару", http.StatusUnauthorized)
-			return
-		}
-		next.ServeHTTP(w, r)
+		http.Error(w, "Не удалось обновить пару токенов", http.StatusUnauthorized)
+		return
 	})
 }
