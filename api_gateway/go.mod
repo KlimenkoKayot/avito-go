cc1c2a26ac71768d8e0c75b2e5d838f699fbfa48
Klimenko George <george_klimenko6@mail.ru>
2025-04-27 23:29:23 +0300

    Фундаментальная структура AuthClient
    Логгер и конфиг по дефолту, добавил контексты, т.к. стал умнее

--- api_gateway/go.mod
+++ api_gateway/go.mod
@@ -1,3 +1,12 @@
 module github.com/klimenkokayot/avito-go/api_gateway
 
 go 1.23.8
+
+require github.com/klimenkokayot/avito-go/libs/logger v0.0.0-20250427201639-306c91fab6f1
+
+require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- api_gateway/internal/infrastructure/http/client/auth_client.go
+++ api_gateway/internal/infrastructure/http/client/auth_client.go
@@ -1,1 +1,37 @@
 package client
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/klimenkokayot/avito-go/api_gateway/config"
+	model "github.com/klimenkokayot/avito-go/api_gateway/internal/domain/model/auth"
+	"github.com/klimenkokayot/avito-go/api_gateway/internal/domain/ports"
+	"github.com/klimenkokayot/avito-go/libs/logger"
+)
+
+type AuthClient struct {
+	client      *http.Client
+	authBaseURL string
+	logger      *logger.Logger
+	cfg         *config.Config
+}
+
+// Проксирует запросы в микросервис авторизации
+func (a *AuthClient) ProxyAuthRequest(ctx context.Context, r *http.Request) (*http.Response, error) {
+	return nil, nil
+}
+
+// Проверяет пару токенов
+func (a *AuthClient) VerifyTokenPair(ctx context.Context, tokenPair *model.TokenPair) (userID string, err error) {
+	return "", nil
+}
+
+func NewAuthClient(authBaseURL string, logger *logger.Logger, cfg *config.Config) (ports.AuthService, error) {
+	return &AuthClient{
+		client:      &http.Client{},
+		authBaseURL: authBaseURL,
+		logger:      logger,
+		cfg:         cfg,
+	}, nil
+}
