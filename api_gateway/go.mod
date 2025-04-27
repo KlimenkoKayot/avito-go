306c91fab6f19320bfe0022715aa65edc40f9ed9
Klimenko George <george_klimenko6@mail.ru>
2025-04-27 23:16:39 +0300

    Абстракция AuthService
    Для последующей реализации AuthClient

---
+++ api_gateway/go.mod
@@ -0,0 +1,3 @@
+module github.com/klimenkokayot/avito-go/api_gateway
+
+go 1.23.8
--- api_gateway/internal/domain/ports/auth_port.go
+++ api_gateway/internal/domain/ports/auth_port.go
@@ -1,1 +1,15 @@
-package domain
+package ports
+
+import (
+	"context"
+	"net/http"
+
+	model "github.com/klimenkokayot/avito-go/api_gateway/internal/domain/model/auth"
+)
+
+type AuthService interface {
+	ForwardAuthRequest(ctx context.Context, r *http.Request) (*http.Response, error)
+	VerifyTokenPair(ctx context.Context, tokenPair *model.TokenPair) (bool, error)
+	RefreshTokenPair(ctx context.Context, refreshToken string) (*model.TokenPair, error)
+	Logout(ctx context.Context, accessToken string) error // aka инвалидация токенов
+}
